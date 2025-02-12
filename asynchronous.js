804a2a1b5b72aa4699e39c38a966d74c6639bd6c
sunnysingh02 <sunny2005sin@gmail.com>
2025-02-12 15:59:52 +0530

    read file
    reading a file

--- asynchronous.js
+++ asynchronous.js
@@ -13,9 +13,16 @@
 // const ans3=sum(10000);
 // console.log(ans3);
 // //reading a feil
+// const fs=require("fs");
+// const contents=fs.readFileSync("a.txt","utf-8");//bytes,hex
+// console.log(contents);
+// const fs=require("fs");
+// const data=fs.readFileSync("b.txt","utf-8");
+// console.log(data);
 const fs=require("fs");
-const contents=fs.readFileSync("a.txt","utf-8");//bytes,hex
+
+const contents=fs.readFile("a.txt","utf-8");//async
 console.log(contents);
-const fs=require("fs");
-const data=fs.readFileSync("b.txt","utf-8");
-console.log(data);
+
+const contents2=fs.readFileSync("b.txt","utf-8");//synch
+console.log(contents2);
