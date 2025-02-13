ac0743ed2973732eabf78f513bafac23fad3a7bb
sunnysingh02 <sunny2005sin@gmail.com>
2025-02-13 15:42:27 +0530

    Update promises.js

--- promises.js
+++ promises.js
@@ -1,6 +1,23 @@
-function logName(){
-    console.log("sunny singh");
-}
-setTimeout(logName,3000);
-//callback based approach
-//promises based approach
+// function logName(){
+//     console.log("sunny singh");
+// }
+// setTimeout(logName,3000);
+// //callback based approach
+// //promises based approach
+// function setTimeoutPromisified(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+//   }
+//   //returns an object of the promise class
+//   function callback() {
+//       console.log("3 seconds have passed");
+//   }
+  
+//   setTimeoutPromisified(3000).then(callback)
+
+  function random(){
+
+  }
+  
+  let p=new Promise(random)
+  console.log(p);
+  
